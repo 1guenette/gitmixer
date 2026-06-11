8040309bd6743cc4c7003b26c2280b9a2108fd4c
sguenette <sguenette@versay.com>
2026-06-11 14:53:17 -0500

    mod

--- src/gix/gix.py
+++ src/gix/gix.py
@@ -4,7 +4,7 @@ import subprocess
 from github import Github, Auth
 import os
 from .gix_cli.cmd_gix import GixCMD
-
+import random
 
 def main():
     parser = argparse.ArgumentParser(prog="gix", description="Git mixer utility")
@@ -27,17 +27,18 @@ def main():
 
     print(f"******Main argument: {parsed_args}")
     process(parsed_args.arg)
+    
     # for repo in g.get_user().get_repos():
     #     print(repo.name)
 
 def process(cmd_list:list[str]):
     git_cmd = cmd_list[0]
     if git_cmd == 'commit':
-        print("========PROCESSING")
-        print(cmd_list)
         #subprocess.run(["git"] + cmd_list)
         subprocess.run( ['git', 'commit', '--amend', "--author=\"gix <gix@gix.com>\"", "--no-edit"])
         subprocess.run(['git', 'commit', '--amend', '--author=gix <gix@gix.com>', '--no-edit'])
-
+    elif git_cmd == 'test':
+        print("TEST")
+        
     else:
         subprocess.run(["git"] + cmd_list)
