35d76655875a3974c3a318e19211dc5c0b103918
gix <gix@gix.com>
2026-06-11 16:45:02 -0500

    mod

--- src/gix/gix.py
+++ src/gix/gix.py
@@ -1,7 +1,8 @@
 import argparse
 from typing import Text
 import subprocess
-from github import Github, Auth
+from github import Github, Auth, Repository
+from git import Repo
 import os
 from .gix_cli.cmd_gix import GixCMD
 import random
@@ -34,11 +35,18 @@ def main():
 def process(cmd_list:list[str]):
     git_cmd = cmd_list[0]
     if git_cmd == 'commit':
+        #TODO
         #subprocess.run(["git"] + cmd_list)
-        subprocess.run( ['git', 'commit', '--amend', "--author=\"gix <gix@gix.com>\"", "--no-edit"])
+        #subprocess.run( ['git', 'commit', '--amend', "--author=\"gix <gix@gix.com>\"", "--no-edit"])
         subprocess.run(['git', 'commit', '--amend', '--author=gix <gix@gix.com>', '--no-edit'])
     elif git_cmd == 'test':
         print("TEST")
+    elif git_cmd == 'init':
+        #TODO
+        subprocess.run(["git"] + cmd_list)
+    elif git_cmd == 'push':
+        #TODO
+        subprocess.run(["git"] + cmd_list)
         
     else:
         subprocess.run(["git"] + cmd_list)
--- src/gix/gix_cli/cmd_gix.py
+++ src/gix/gix_cli/cmd_gix.py
@@ -1,6 +1,8 @@
 
 config_options = ['token', 'accounts']
 config_options = ['remote', 'local']
+import subprocess
+
 class GixCMD():
     #TODO: Implement custom git/gitx config commands
 
@@ -10,8 +12,10 @@ class GixCMD():
     def process(self, cmd):
         print("....Processing")
         if cmd == 'commit':
-            #git commit then
-            #git commit --amend --author="John Doe <john.doe@fu.com>" --no-edit
+            print("========PROCESSING")
+            #subprocess.run(["git"] + cmd_list)
+            #subprocess.run( ['git', 'commit', '--amend', "--author=\"gix <gix@gix.com>\"", "--no-edit"])
+            subprocess.run(['git', 'commit', '--amend', '--author=gix <gix@gix.com>', '--no-edit'])
 
             self.commit()
         else:
